3c8877beb03f0a65b918693c1dde3ce39f9d247b
Roberto Baito <robert.baito@innohub.ph>
2014-02-28 17:50:07 +0800

    added factory_girl

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jquery-rails'
 
 group :development, :test do
     gem 'rspec-rails', '~> 3.0.0.beta'
+    gem "factory_girl"
 end
 
 # To use ActiveModel has_secure_password
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,4 +34,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #factorygirl
+  config.include FactoryGirl::Syntax::Methods
 end
