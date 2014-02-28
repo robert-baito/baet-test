c57a4995b613be188f66a7cda5a31b76cc358585
Roberto Baito <robert.baito@innohub.ph>
2014-02-28 13:59:19 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaetTest::Application.config.secret_token = 'e6b07c05ecc587deadd104108e3ca3e3a4b00d20d159be8a498e5232a6ef825daa960fdbfad573bbd0a38d6ba2de2c8f5c1604731beff32de406cf65aec5a1dd'
